35bd0cd2f86cd0c938bcc658c3fd21857e52812a
Lucas Galdino <lucaas.galdinno@outlook.com>
2023-05-02 10:01:30 -0300

    Add files via upload

---
+++ Aula03_22_09_2020/Aula03_22_09_2020/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Aula03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Não sei o que escrever agora!!!");
+            Console.WriteLine("Feliz Natal!!!");
+        }
+    }
+}
