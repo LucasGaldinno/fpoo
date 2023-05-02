3cb05cd05efd7338f9239d8e5cbf86c08f2146ef
Lucas Galdino <lucaas.galdinno@outlook.com>
2023-05-02 10:24:26 -0300

    Add files via upload

---
+++ Aula27_17_12_2020/C#/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] vetorA = new double[4];
+            double[] vetorB = new double[4];
+
+            for(int i = 0; i < vetorA.Length; i++){
+                Console.Write("Digite um valor para o vetorA: ");
+                vetorA[i] = Convert.ToDouble(Console.ReadLine());
+
+                vetorB[i] = Math.Pow(vetorA[i], 2);
+                //vetorB[i] = vetorA[i] * vetorA[i];
+            }
+
+            for(int i = 0; i < vetorA.Length; i++){
+                Console.WriteLine($"vetorA[{i}] = {vetorA[i]} \t vetorB[{i}] = {vetorB[i]}");
+            }
+               
+        }
+    }
+}
---
+++ Aula27_17_12_2020/C#/diagonal_secundaria/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace diagonal_secundaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[3,3];
+
+            //criar a linha
+            for(int i = 0; i < 3; i++){
+                //criar a coluna
+                for(int j = 0; j < 3; j++){
+                    Console.Write("Digite um valor para a matriz[{0},{1}]: ", i, j);
+                    matriz[i,j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            for(int i = 0; i < 3; i++){
+                for(int j = 0; j < 3; j++){
+                    if((i+j) == 2){
+                        Console.WriteLine($"matriz[{i},{j}] = {matriz[i,j]}");
+                    }
+                }
+            }
+        }
+    }
+}
