56157b784b2a7201eeb5ee6d8fef34d9feb301d2
Lucas Galdino <lucaas.galdinno@outlook.com>
2023-05-02 10:03:39 -0300

    Add files via upload

---
+++ Aula06_01_10_2020/Aula06_01_10_2020/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Aula06_01_10_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declarar variaveis
+            float c;
+            float f;
+
+            //entrada(s)
+            Console.Write("Digite a temperatura da sua cidade: ");
+            c = Convert.ToSingle(Console.ReadLine());
+
+            //processamento
+            f = (9 * c + 160) / 5;
+
+            //saída(s)
+            Console.Write("Esta temperatura em Fahrenheit é: " + f);
+        }
+    }
+}
