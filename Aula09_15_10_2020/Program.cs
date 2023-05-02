cd2d170ba06d64a9b8086398625da5616d708cc3
Lucas Galdino <lucaas.galdinno@outlook.com>
2023-05-02 10:04:29 -0300

    Add files via upload

---
+++ Aula09_15_10_2020/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Aula08_15_out_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declarar variaveis
+            float tempo, vel_media, dist, litros_usados;
+
+            //entrada
+            Console.Write("Digite o tempo gasto na viagem: ");
+            tempo = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite a velocidade média gasta na viagem: ");
+            vel_media = float.Parse(Console.ReadLine());
+
+            //processamento
+            dist = tempo * vel_media;
+            litros_usados = dist / 12;
+
+            //saida
+            Console.WriteLine("A velocidade média é: " + Math.Round(vel_media, 2));
+            Console.WriteLine("O tempo gasto na viagem é: " + Math.Round(tempo, 2));
+            Console.WriteLine("A distância gasta na viagem é: " + Math.Round(dist, 2));
+            Console.WriteLine("Os litros gastos na viagem são: " + Math.Round(litros_usados, 2));
+
+        }
+    }
+}
