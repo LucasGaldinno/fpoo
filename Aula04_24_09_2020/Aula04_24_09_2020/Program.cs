b4b2056cf81dd71fdf1585c3af2abc5cf7a32e5e
Lucas Galdino <lucaas.galdinno@outlook.com>
2023-05-02 10:02:21 -0300

    Add files via upload

---
+++ Aula04_24_09_2020/Aula04_24_09_2020/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula04_24_09_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
