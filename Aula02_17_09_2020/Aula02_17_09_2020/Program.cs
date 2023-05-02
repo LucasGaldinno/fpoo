c3ff764d0c1e826925f433afa0c692b701615c42
Lucas Galdino <lucaas.galdinno@outlook.com>
2023-05-02 10:00:56 -0300

    Add files via upload

---
+++ Aula02_17_09_2020/Aula02_17_09_2020/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Aula02_17_09_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vai curintia!!!");
+            Console.WriteLine("Palmeiras não tem mundial!!!");
+            Console.WriteLine("São Paulo não tem Copa do Brasil!!!");
+            Console.Write("Santos é o Santos!!!");
+        }
+    }
+}
