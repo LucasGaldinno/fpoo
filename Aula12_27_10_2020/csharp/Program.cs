6812759aee5da55c908fdbab47abc5256b6de3d0
Lucas Galdino <lucaas.galdinno@outlook.com>
2023-05-02 10:09:25 -0300

    Add files via upload

---
+++ Aula12_27_10_2020/csharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+
+            Console.Write("Digite sua idade: ");
+            idade = Convert.ToInt32(Console.ReadLine());
+
+            if (idade >= 18)
+            {
+                Console.Write("Você é maior de idade...");
+            }
+            else
+            {
+                Console.Write("Você é menor de idade...");
+                
+            }
+        }
+    }
+}
