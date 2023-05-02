b59af5f8d341103a254dd4e2625bc8e414cc8dfa
Lucas Galdino <lucaas.galdinno@outlook.com>
2023-05-02 10:10:00 -0300

    Add files via upload

---
+++ Aula13_29_10_2020/csharp/Exemplo02.cs
@@ -0,0 +1,43 @@
+// using System;
+
+// namespace csharp
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             //exemplo de estrutura de decisão simples
+            
+//             //declarar variaveis
+//             float n1, n2, n3, n4, mf;
+
+//             //entradas
+//             Console.Write("Digite a primeira nota bimestral: ");
+//             n1 = float.Parse(Console.ReadLine());
+
+//             Console.Write("Digite a segunda nota bimestral: ");
+//             n2 = float.Parse(Console.ReadLine());
+
+//             Console.Write("Digite a terceira nota bimestral: ");
+//             n3 = float.Parse(Console.ReadLine());
+            
+//             Console.Write("Digite a quarta nota bimestral: ");
+//             n4 = float.Parse(Console.ReadLine());
+
+//             //processamento
+//             mf = (n1 + n2 + n3 + n4) / 4;
+
+//             //saida
+//             if (mf >= 5)
+//             {
+//                 Console.WriteLine("Você está aprovado");
+//             }
+//             else
+//             {
+//                 Console.WriteLine("Você está reprovado");
+//             }
+
+//             Console.Write("\n\nFim do programa!!!");
+//         }
+//     }
+// }
---
+++ Aula13_29_10_2020/csharp/Exemplo03.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //exemplo de estrutura de decisão simples
+            
+            //declarar variaveis
+            float n1, n2, n3, n4, mf;
+
+            //entradas
+            Console.Write("Digite a primeira nota bimestral: ");
+            n1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite a segunda nota bimestral: ");
+            n2 = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite a terceira nota bimestral: ");
+            n3 = float.Parse(Console.ReadLine());
+            
+            Console.Write("Digite a quarta nota bimestral: ");
+            n4 = float.Parse(Console.ReadLine());
+
+            //processamento
+            mf = (n1 + n2 + n3 + n4) / 4;
+
+            //saida
+            if (mf >= 7)
+            {
+                Console.WriteLine("Você está aprovado");
+            }
+            else if (mf < 5)
+            {
+                Console.WriteLine("Você está reprovado");                
+            }
+            else
+            {
+                Console.WriteLine("Você está de recuperação");                                
+            }
+
+            Console.Write("\n\nFim do programa!!!");
+        }
+    }
+}
